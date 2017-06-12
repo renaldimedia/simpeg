f6858c13b98723b8c3c90af7bdde97e4c817854f
ardi renaldi <renaldimedia@gmail.com>
2017-06-12 10:52:47 +0700

    first commit

---
+++ application/controllers/Main.php
@@ -0,0 +1,42 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+ 
+class Main extends CI_Controller {
+ 
+function __construct()
+{
+        parent::__construct();
+ 
+/* Standard Libraries of codeigniter are required */
+$this->load->database();
+$this->load->helper('url');
+/* ------------------ */ 
+ 
+$this->load->library('grocery_CRUD');
+ 
+}
+ 
+public function index()
+{
+echo "<h1>Welcome to the world of Codeigniter</h1>";//Just an example to ensure that we get into the function
+die();
+}
+public function pegawai()
+{
+$crud = new grocery_CRUD();
+$crud->set_table('pegawai');
+$crud->set_relation('id_satker','satuan_kerja','pegawai');
+$output = $this->grocery_crud->render();
+$this->_example_output($output);      
+// echo "<pre>";
+// print_r($output);
+// echo "</pre>";
+// die();
+}
+function _example_output($output = null)
+ 
+{
+$this->load->view('our_template.php',$output);    
+}
+} 
+/* End of file Main.php */
+/* Location: ./application/controllers/Main.php */
